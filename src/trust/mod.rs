011d47f88d0ee47aad9a5935717c8a9a9920e75e
Killavus <killavus@gmail.com>
2023-03-18 20:46:43 +0000

    fix typo with module name (conditional compilation sucks)

--- src/trust/mod.rs
+++ src/trust/mod.rs
@@ -6,7 +6,7 @@ pub mod win32_store;
 pub fn install_cert_on_machine(cert: &Certificate) -> AppResult<()> {
     #[cfg(windows)]
     {
-        let store = win32store::TrustStore::open()?;
+        let store = win32_store::TrustStore::open()?;
         store.install(cert)?;
     }
 
